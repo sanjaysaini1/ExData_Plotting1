2514c149b5e0d33a7c23f0de417b920a78cc309c
Sanjay Saini <sanjaysaini1@gmail.com>
2015-03-07 21:48:28 +0530

    Course Project 1

--- plot1.R
+++ plot1.R
@@ -4,7 +4,6 @@ data<-read.table("household_power_consumption.txt",sep=";",na.strings="?",header
 data$Date<-as.Date(data$Date,format="%d/%m/%Y")
 # read data for dates 2007/02/01 and 2007/02/02
 mydata<-data[data$Date %in% as.Date(c('2007-02-01','2007-02-02')),]
-# set background as white else image is transparent background
 
 #save image as png, default values are 480px width and height with white background
 png("plot1.png")
--- plot4.R
+++ plot4.R
@@ -4,14 +4,15 @@ data<-read.table("household_power_consumption.txt",sep=";",na.strings="?",header
 data$Date<-as.Date(data$Date,format="%d/%m/%Y")
 # read data for dates 2007/02/01 and 2007/02/02
 mydata<-data[data$Date %in% as.Date(c('2007-02-01','2007-02-02')),]
+#concatenate time to date and put it in POSIXct notation
+mydata$Date<-as.POSIXct(paste(as.character(mydata$Date),as.character(mydata$Time)),"%d-%m-%Y %H:%M:%S")
+
 
 #save image as png, default values are 480px width and height with white background
 png("plot4.png")
 ##Draw plot
 # set 2 row and 2 columns for 4 plots
 par(mfrow=c(2,2))        
-#concatenate time to date and put it in POSIXct notation
-mydata$Date<-as.POSIXct(paste(as.character(mydata$Date),as.character(mydata$Time)),"%d-%m-%Y %H:%M:%S")
 #create 1st plot
 plot(mydata$Global_active_power~mydata$Date,type="l",xlab="",ylab="Global Active Power(kilowatts)")
 #create 2nd plot
